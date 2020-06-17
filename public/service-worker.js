940ce8e7e0f1cc63bee71a7ae654055db645d745
OliwerDrywa <gitoliwer@gmail.com>
2020-06-17 11:55:57 +0100

    implements workbox for service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,86 +1,24 @@
-const staticCache = 'static-v0.0'
-const dynamicCache = 'dynamic-v0.0'
-const assets = [
-  '/',
-  '/index.html',
-  '/global.css',
-  '/build/bundle.css',
-  '/build/bundle.js',
-  'https://fonts.googleapis.com/css?family=Oswald|Roboto&display=swap',
-]
+const cacheName = 'stale-with-revalidate'
 
-const precache = async () => {
-  const cache = await caches.open(staticCache)
-  cache.addAll(assets)
-}
+// import workbox 
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
+const { routing, strategies } = workbox
 
-// install
-self.addEventListener('install', (e) => e.waitUntil(precache()))
+// implements staleWhileRevalidate to all routes
+routing.registerRoute(
+  () => true,
+  new strategies.StaleWhileRevalidate({ cacheName }),
+)
 
+
+// removes all caches not named <cacheName>
 const invalidateOldCache = async () => {
   const keys = await caches.keys()
-  const isOldCache = (key) => key !== staticCache && key !== dynamicCache
+  const isOldCache = (key) => key !== cacheName
   const oldKeys = keys.filter(isOldCache)
+
   return Promise.all(oldKeys.map((key) => caches.delete(key)))
 }
 
+// runs invalidateOldCache on activation
 self.addEventListener('activate', (e) => e.waitUntil(invalidateOldCache()))
-
-const fetchCache = async (request) => {
-  const cacheResponse = await caches.match(request)
-  return (
-    cacheResponse ||
-    fetch(request).then(async (fetchRes) => {
-      const cache = await caches.open(dynamicCache)
-      cache.put(request.url, fetchRes.clone())
-
-      return fetchRes
-    })
-  )
-}
-
-const serveFallback = () => {
-  //   caches.match('/fallback.html')
-}
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetchCache(e.request).catch(serveFallback))
-})
-
-// var cacheName = "sgtoilet-cache-" + Date.now();
-// var filesToCache = [
-//   "/",
-//   "/index.html",
-//   "/main.css",
-//   "/main.js",
-//   "/components.css",
-//   "https://fonts.googleapis.com/css?family=Oswald|Roboto&display=swap"
-// ];
-// self.addEventListener("install", function(e) {
-//   e.waitUntil(
-//     caches.open(cacheName).then(function(cache) {
-//       return cache.addAll(filesToCache);
-//     })
-//   );
-// });
-// self.addEventListener("activate", e => {
-//   e.waitUntil(
-//     caches.keys().then(function(cacheNames) {
-//       return Promise.all(
-//         cacheNames.map(function(thisCacheName) {
-//           if (thisCacheName !== cacheName) {
-//             return caches.delete(thisCacheName);
-//           }
-//         })
-//       );
-//     })
-//   );
-// });
-// self.addEventListener("fetch", e => {
-//   e.respondWith(
-//     (async function() {
-//       const response = await caches.match(e.request);
-//       return response || fetch(e.request);
-//     })()
-//   );
-// });
