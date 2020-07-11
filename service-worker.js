eda3b80033492ef914d04c2a11f3ba858b739264
Todor Imreorov <blurymind@gmail.com>
2020-07-11 18:19:09 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,25 @@
-const {precacheAndRoute} = require('workbox-precaching');
-const { NetworkFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
+const cacheName = 'stale-with-revalidate'
 
-registerRoute(/https:\/\/yarnspinnertool\.github\.io\/YarnEditor\//, new NetworkFirst());
+// import workbox 
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
+const { routing, strategies } = workbox
+
+// implements staleWhileRevalidate to all routes
+routing.registerRoute(
+  () => true,
+  new strategies.StaleWhileRevalidate({ cacheName }),
+)
+
+
+// removes all caches not named <cacheName>
+const invalidateOldCache = async () => {
+  const keys = await caches.keys()
+  const isOldCache = (key) => key !== cacheName
+  const oldKeys = keys.filter(isOldCache)
+
+  return Promise.all(oldKeys.map((key) => caches.delete(key)))
+}
+
+// runs invalidateOldCache on activation
+self.addEventListener('activate', (e) => e.waitUntil(invalidateOldCache()))
 precacheAndRoute([{"revision":"816d7badea7192bd75aa5239f8219212","url":"build/bundle.css"},{"revision":"670a9ea478259fa8c750e86abe16e717","url":"build/bundle.js"},{"revision":"3b8d31e2dc3f8d2f71ab725efcca7c9f","url":"global.css"},{"revision":"992e6c258efde68eff7fb5e09f3ccbb6","url":"index.html"}]);
