cd4043fb6c87dee59626b68d9c3390a6064e7bd8
Todor Imreorov <blurymind@gmail.com>
2020-07-11 18:18:05 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,5 @@ const {precacheAndRoute} = require('workbox-precaching');
 const { NetworkFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 
-console.log('Yarn\'s service worker is caching files');
 registerRoute(/https:\/\/yarnspinnertool\.github\.io\/YarnEditor\//, new NetworkFirst());
 precacheAndRoute([{"revision":"816d7badea7192bd75aa5239f8219212","url":"build/bundle.css"},{"revision":"670a9ea478259fa8c750e86abe16e717","url":"build/bundle.js"},{"revision":"3b8d31e2dc3f8d2f71ab725efcca7c9f","url":"global.css"},{"revision":"992e6c258efde68eff7fb5e09f3ccbb6","url":"index.html"}]);
